e28d137c255becf12be02380e96d280fd91217cb
pandaycp <33088596+pandaycp@users.noreply.github.com>
2024-07-29 13:02:14 +0530

    Update 2nd commit

--- TestJenkins.java
+++ TestJenkins.java
@@ -2,6 +2,6 @@ public class TestJenkins{
     public static void main(String[]args){
         System.out.println("Hello, this is my first java program for Jenkins !!!");
         System.out.println("Hi");
-        System.out.println("A new line added");
+        System.out.println("A new line added1");
     }
 }
